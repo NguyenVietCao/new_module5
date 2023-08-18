0f9eb4bd6c7c41148cd7a25472d0824e98f60880
cao <vietcao2510@gmail.com>
2023-08-18 11:49:51 +0700

    fix es6

--- ES6/ss1.js
+++ ES6/ss1.js
@@ -24,11 +24,12 @@ let courses = [
       title: "JavaScript Tutorial", 
       rating: 3.5, 
     }, 
-  ];
+];
+  // xuat lon hon 4
   let arr = courses.filter((element) => (element.rating >=4));
   console.log(arr);
-
-  let arr1 = courses.filter((element) =>(element.rating >=4)).map((el) =>(el.id +" - "+ el.title +" - "+el.rating))
+// nho hon 4 voi dinh dang 
+  let arr1 = courses.filter((element) =>(element.rating <4)).map((el) =>(el.id +" - "+ el.title +" - "+el.rating))
 console.log(arr1);
 
 let addedCourses = [ 
@@ -50,5 +51,5 @@ let addedCourses = [
   ]; 
   
 
-  let sum = (a,b)=>[...a,...b];
-  console.log(sum(addedCourses,courses));
+  let mergeArray = (a,b)=>[...a,...b];
+console.log(mergeArray(addedCourses,courses));
